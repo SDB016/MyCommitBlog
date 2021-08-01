6438fba1b54d900c2d82422258eda9fb5a882564
SDB016 <dongbin4013@naver.com>
2021-08-01 20:35:10 +0900

    post model  & posts view update

--- myCommitBlog/page/models.py
+++ myCommitBlog/page/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    content = models.TextField()
+    writer = models.CharField(max_length=20, null=False)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    updatedDate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.content[:100]
+
--- myCommitBlog/page/views.py
+++ myCommitBlog/page/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
+from .models import Post
 
 def home(request):
-    return render(request, "home.html")
+    posts = Post.objects.all()
+    return render(request, "home.html", {'posts': posts})
 
 
 def posts(request):
-    return render(request, "posts.html")
+    posts = Post.objects.all()
+    return render(request, "posts.html", {'posts': posts})
 
 
 def posting(request):
